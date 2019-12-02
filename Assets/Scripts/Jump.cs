e1a48aa7bff00f8df5126e40c82f66f019bd1f51
yusufalibrahim1994 <alibrahim1994@live.com>
2019-12-02 20:32:24 +0300

    Animation and movement working
    jumping, working, falling all work!

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    private CharacterController controller;
+    private float verticalVelocity;
+    private float gravity = 14.0f;
+    private float jumpForce = 10.0f;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if(controller.isGrounded)
+        {
+            verticalVelocity = -gravity * Time.deltaTime;
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                verticalVelocity = jumpForce;
+            }
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+        Vector3 moveVector = new Vector3(0, verticalVelocity, 0);
+        controller.Move(moveVector * Time.deltaTime);
+    }
+    
+}
+
---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    //SerializeFields
+    [SerializeField] Transform startCube;
+    [SerializeField] Transform endCube;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "start")
+        {
+            print("it touched");
+            gameObject.transform.position = endCube.transform.position + new Vector3(0, -16.2f, 30);
+            return;
+        }
+
+        if (col.tag == "end")
+        {
+            gameObject.transform.position = startCube.transform.position + new Vector3(0, -16.2f, -30);
+            return;
+        }
+    }
+}

e46ecb1ea793e83dc9d5498b38a6cfee7d0783a6
yusufalibrahim1994 <alibrahim1994@live.com>
2019-12-08 20:21:15 +0300

    Android support

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject Player;
+    public float movementSpeed = 4;
+    void Update()
+    {
+        transform.LookAt(Player.transform);
+        transform.position += transform.forward * movementSpeed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/enemyAI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyAI : MonoBehaviour
+{
+    public Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(playerTransform.position, this.transform.position) < 10)
+        {
+            Vector3 direction = playerTransform.position - this.transform.position;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+        }
+    }
+}
